b47e25acd27d0fd0ec7e8649440e4a1b4868a22a
hopefulmule <chris.hansen@modernmi.com>
2016-08-08 18:57:18 -0600

    Lesson 8 end

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Angular E2E Testing Guide:
+// https://docs.angularjs.org/guide/e2d-testing
+
 describe('PhoneCat Application', function() {
 
   describe('phoneList', function(){
@@ -12,14 +15,14 @@ describe('PhoneCat Application', function() {
           var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
           var query = element(by.model('$ctrl.query'));
 
-          expect(phoneList.count()).toBe(3);
+          expect(phoneList.count()).toBe(20);
 
           query.sendKeys('nexus');
           expect(phoneList.count()).toBe(1);
 
           query.clear();
           query.sendKeys('motorola');
-          expect(phoneList.count()).toBe(2);
+          expect(phoneList.count()).toBe(8);
       });
 
       it('should be possible to control phone order via the drop-down menu', function() {
@@ -34,21 +37,29 @@ describe('PhoneCat Application', function() {
               });
           }
 
-          queryField.sendKeys('tablet'); //narrow dataset
+          queryField.sendKeys('tablet'); // Let's narrow the dataset to make assertions shorter
 
           expect(getNames()).toEqual([
-              'Motorola XOOM with Wi-Fi',
-              'MOTOROLA XOOM'
+              'Motorola XOOM\u2122 with Wi-Fi',
+              'MOTOROLA XOOM\u2122'
           ]);
 
           nameOption.click();
 
           expect(getNames()).toEqual([
-              'MOTOROLA XOOM',
-              'Motorola XOOM with Wi-Fi'
+              'MOTOROLA XOOM\u2122',
+              'Motorola XOOM\u2122 with Wi-Fi'
           ]);
-
       });
+
+      it('should render phone specific links', function() {
+          var query = element(by.model('$ctrl.query'));
+          query.sendKeys('nexus');
+
+          element.all(by.css('.phones li a')).first().click();
+          expect(browser.getLocationAbsUrl()).toBe('/phones/nexus-s');
+      });
+
   });
 
 });
