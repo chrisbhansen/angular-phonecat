912d2ec612859e901a53398471d2afd6e8bfb1f4
hopefulmule <chris.hansen@modernmi.com>
2016-08-09 16:29:43 -0600

    Lesson 12 end

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -1,17 +1,24 @@
 'use strict';
 
-// Register 'phoneDetail' component, along with its associated controller and template
+// Register `phoneDetail` component, along with its associated controller and template
 angular.
-    module('phoneDetail').
-    component('phoneDetail', {
-        templateUrl: 'phone-detail/phone-detail.template.html',
-        controller: ['$http', '$routeParams',
-            function PhoneDetailController($http, $routeParams) {
-                var self = this;
+module('phoneDetail').
+component('phoneDetail', {
+    templateUrl: 'phone-detail/phone-detail.template.html',
+    controller: ['$http', '$routeParams',
+        function PhoneDetailController($http, $routeParams) {
+            var self = this;
 
-                $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response){
-                    self.phone = response.data;
-                });
-            }
-        ]
-    });
+            self.setImage = function setImage(imageUrl) {
+                self.mainImageUrl = imageUrl;
+            };
+
+            $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
+                self.phone = response.data;
+                self.setImage(self.phone.images[0]);
+            });
+
+        }
+    ]
+});
+
